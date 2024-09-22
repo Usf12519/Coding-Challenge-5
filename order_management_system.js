b4ed1b890103125f28523dc403f5ca91b6c29eb8
Cameron Smith <Cs11@usf.edu>
2024-09-22 14:41:21 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,10 @@
+// Task 1: Create an Inventory Array of Producy Objects
+
+const inventory = [
+    {name: 'Bed', price: 2700, quantity: 5},
+    {name: 'Chair', price: 700, quantity: 3},
+    {name: 'Lamp', price: 300, quantity: 2},
+    {name: 'Dresser', price: 650, quantity: 4},
+    
+];
+
